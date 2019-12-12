6cdc2fb8571e9fcc1e5887bd9abfb9a6dec0eca4
Twynzen <razzziel001@gmail.com>
2019-12-12 12:19:53 -0500

    Cree la conexión a la base de datos

---
+++ Utils/ConexionDB.php
@@ -0,0 +1,14 @@
+<?php
+
+class ConexionDB{
+
+    //El metodo que conecta a la base de datos
+    public function getConexion(){
+
+        $cnx = new PDO ("mysql:host=localhost;dbname=tienda","root","");
+        return $cnx;
+    }
+
+}
+
+?>
