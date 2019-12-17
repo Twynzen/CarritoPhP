8eec01ecd4d7e493ea6ca750e4d1210b3b71dee3
Daniel Castiblanco <razzziel001@gmail.com>
2019-12-16 21:51:47 -0500

    Metodos DAO creamos un metodo que guarde los productos de la base de datos en una lista

---
+++ DAO/MetodosDAO.php
@@ -0,0 +1,22 @@
+<?php
+
+include '../Utils/ConexionDB.php';
+
+class MetodosDAO {
+    public function listarProductos(){
+        $cnx = new ConexionBD();
+        $cn= $cnx -> getConexion();
+
+        $res = $cn-> prepare("select * from productos");
+        $res -> execute():
+
+        foreach ($res as $row){
+            $lista[]=$row;
+        }
+        return $lista;
+        //conseguimos todos los productos de la tabla productos y los guardamos 
+        //en lista
+    }
+}
+
+?>
